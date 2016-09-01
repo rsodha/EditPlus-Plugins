cd64d52d340476e5db6648e0311438eb1359b4a3
Raghu Sodha <raghu.d.sodha@gmail.com>
2016-09-01 16:45:16 -0400

    XML format plugin

---
+++ EditPlus_Plugin_XML_Format.ps1
@@ -0,0 +1,16 @@
+﻿function Format-XML {
+  [CmdletBinding()]
+  Param ([Parameter(ValueFromPipeline=$true,Mandatory=$true)][string]$xmlcontent)
+  $xmldoc = New-Object -TypeName System.Xml.XmlDocument
+  $xmldoc.LoadXml($xmlcontent)
+  $sw = New-Object System.IO.StringWriter
+  $writer = New-Object System.Xml.XmlTextwriter($sw)
+  $writer.Formatting = [System.XML.Formatting]::Indented
+  $xmldoc.WriteContentTo($writer)
+  $sw.ToString()
+}
+
+$FilePath = $args[0];
+$Lines = Get-Content $FilePath;
+
+echo $Lines | Format-XML
